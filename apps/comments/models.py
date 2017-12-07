2d698fdb1009a5087988987915655ca98f9fca3e
mayouzi <mayormail@qq.com>
2017-12-07 11:41:32 +0800

    submit static path settings

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 from django.utils.six import python_2_unicode_compatible
-from apps.blog.models import Post
 
 # python_2_unicode_compatible 装饰器用于兼容 Python2
 @python_2_unicode_compatible
@@ -14,7 +13,7 @@ class Comment(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
     # post = models.ForeignKey('apps.blog.Post')
-    post = models.ForeignKey('Post')
+    post = models.ForeignKey('blog.Post')
 
     def __str__(self):
         return self.text[:20]
--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.http import HttpResponse
 from apps.blog.feeds import AllPostsRssFeed
 
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('apps.blog.urls')),
     url(r'', include('apps.comments.urls')),
     url(r'^robots\.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
