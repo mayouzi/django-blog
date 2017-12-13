af9477088b071d947332bee3238483b8ea0bca1a
mayouzi <mayormail@qq.com>
2017-12-13 19:12:41 +0800

    code optimize

--- apps/blog/templatetags/blog_tags.py
+++ apps/blog/templatetags/blog_tags.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from django import template
 from django.db.models.aggregates import Count
 
@@ -18,11 +20,9 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    # 记得在顶部引入 count 函数
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
 
 @register.simple_tag
 def get_tags():
-    # 记得在顶部引入 Tag model
     return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
